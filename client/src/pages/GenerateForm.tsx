02a4f739e4e1145dc44a3b20dbddaf8913be3e9b
levikaw <levkin_dmitrii@mail.ru>
2023-12-14 14:47:14 +0500

    fix(form-rile): fix submit

--- client/src/pages/GenerateForm.tsx
+++ client/src/pages/GenerateForm.tsx
@@ -1,16 +1,29 @@
 import AutoModeIcon from '@mui/icons-material/AutoMode';
 import { Typography, Avatar, Button, Box, TextField } from '@mui/material';
-
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import axios from "axios";
 
 export default function GenerateForm(props: any) {
     const userId = props.userId
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        console.log(userId);
-        // send file here
-    }
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+        },
+        onSubmit: (values) => {
+            axios
+                .post(`${process.env.REACT_APP_API_URL}get_answer/`, { userId: userId, ...values })
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().trim(),
+        }),
+    });
 
+
+    const handleGetHistory = () => {
+        // 
+    }
 
     return (
         <Box
@@ -27,7 +40,7 @@ export default function GenerateForm(props: any) {
             <Typography component="h1" variant="h5">
                 Generate NBO
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
                 <TextField
                     margin="normal"
                     required
@@ -45,6 +58,13 @@ export default function GenerateForm(props: any) {
                     Generate
                 </Button>
             </Box>
+            <Button
+                onClick={handleGetHistory}
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Download json history
+            </Button>
         </Box>
     )
 }
